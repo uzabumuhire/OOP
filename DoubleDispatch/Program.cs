74770b561c26b94cb839cbffe86a9c9c3691e864
Jean Jacques Uzabumuhire <uzabumuhire@gmail.com>
2021-10-04 22:50:33 +0200

    Add project files.

---
+++ DoubleDispatch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DoubleDispatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
